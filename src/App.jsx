b65ac36213c8ee498dc104a8ac594074cf57a8c4
cjtrahey <trahey.03@gmail.com>
2022-09-26 22:18:19 -0400

    adding files, made some progress

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+   <div className="min-h-screen flex flex-col text-white">
+     <main className="container mx-auto px-6 pt-16 flex-1 text-center">
+
+     </main>
+
+     <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
+      <p>built by CJ</p>
+      <div className="flex -mx-6">
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">About us</a> |
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">Privacy</a> |
+        <a href="#" className="mx-3 hover:opacity-80 duration-150">Contact</a> 
+      </div>
+     </footer>
+   </div>
+  )
+}
+
+export default App
